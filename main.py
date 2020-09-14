f387556b43bd24924b68d4ff97a8ebd9519296c2
Vladimir Mukhin <vlad@mukhin.page>
2020-09-14 19:10:26 -0400

    Correct order of functions

--- main.py
+++ main.py
@@ -7,40 +7,6 @@ from os import getenv
 import RPi.GPIO as GPIO
 import multiprocessing
 
-jenkins_url      = getenv('JENKINS_URL')
-jenkins_username = getenv('JENKINS_USERNAME')
-jenkins_password = getenv('JENKINS_PASSWORD')
-
-server = jenkins.Jenkins(jenkins_url, username=jenkins_username, password=jenkins_password)
-
-while (True):
-    job_info = server.get_job_info('store-dev')
-    build_info = server.get_build_info('store-dev', job_info["lastBuild"]["number"])
-    proc = multiprocessing.Process(target=enableYellow, args=())
-
-    if build_info['building'] == True:
-        color = 'yellow'
-        build_info['result'] = 'IN PROGRESS'
-        #blink yellow
-        proc.start()
-    elif build_info['result'] == 'SUCCESS':
-        color = 'green'
-        proc.terminate()
-        enableGreen()
-    elif build_info['result'] == 'FAILURE':
-        color = 'red'
-        proc.terminate()
-        enableRed()
-    elif build_info['result'] == 'ABORTED':
-        color = red
-        proc.terminate()
-        enableRed()
-
-    print('\033c')
-    print(colored(build_info['result'], color))
-    
-    sleep(4)
-
 def switch(color, mode):
     if color == 'red':
         pin = 21
@@ -74,3 +40,37 @@ def enableGreen():
     switch('green', 'on')
     switch('yellow', 'off')
     switch('red', 'off')
+
+jenkins_url      = getenv('JENKINS_URL')
+jenkins_username = getenv('JENKINS_USERNAME')
+jenkins_password = getenv('JENKINS_PASSWORD')
+
+server = jenkins.Jenkins(jenkins_url, username=jenkins_username, password=jenkins_password)
+
+while (True):
+    job_info = server.get_job_info('store-dev')
+    build_info = server.get_build_info('store-dev', job_info["lastBuild"]["number"])
+    proc = multiprocessing.Process(target=enableYellow, args=())
+
+    if build_info['building'] == True:
+        color = 'yellow'
+        build_info['result'] = 'IN PROGRESS'
+        #blink yellow
+        proc.start()
+    elif build_info['result'] == 'SUCCESS':
+        color = 'green'
+        proc.terminate()
+        enableGreen()
+    elif build_info['result'] == 'FAILURE':
+        color = 'red'
+        proc.terminate()
+        enableRed()
+    elif build_info['result'] == 'ABORTED':
+        color = red
+        proc.terminate()
+        enableRed()
+
+    print('\033c')
+    print(colored(build_info['result'], color))
+    
+    sleep(4)

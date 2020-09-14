7bbac04435d4602f4ceab6e119cf1ec5b4e70b85
Vladimir Mukhin <vlad@mukhin.page>
2020-09-14 18:17:53 -0400

    Different way to start thread

--- gpio.py
+++ gpio.py
@@ -4,21 +4,13 @@ import threading
 
 GPIO.setmode(GPIO.BOARD)
 
-class myThread (threading.Thread):
-   def __init__(self, threadID, name, counter):
-      threading.Thread.__init__(self)
-      self.threadID = threadID
-      self.name = name
-      self.counter = counter
-   def run(self):
-      blink('yellow')
-
 def blink(color):
     while True:
-        switch('yellow', 'on')
+        switch(color, 'on')
         sleep(1)
-        switch('yellow', 'off')
+        switch(color, 'off')
         sleep(1)
+        global stop_threads 
         if stop_threads: 
             break
 
@@ -45,15 +37,16 @@ def enableRed():
 def enableYellow():
     switch('green', 'off')
     switch('red', 'off')
-    thread1 = myThread(1, "Thread-1", 1)
-    thread1.start()
-    sleep(10)
+    stop_threads = False
+    t1 = threading.Thread(target = blink('yellow')) 
+    t1.start() 
+    time.sleep(10) 
     stop_threads = True
+    t1.join() 
 
 def enableGreen():
     switch('green', 'on')
     switch('yellow', 'off')
     switch('red', 'off')
 
-global stop_threads = False
 enableYellow()

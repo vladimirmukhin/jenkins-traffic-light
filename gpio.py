728e3f26c384a8c5c667fd9a6cc72dacd3735d02
Vladimir Mukhin <vlad@mukhin.page>
2020-09-14 18:09:14 -0400

    Add threads for blinking yellow

--- gpio.py
+++ gpio.py
@@ -1,7 +1,26 @@
 import RPi.GPIO as GPIO
 from time import sleep
+import threading
 
 GPIO.setmode(GPIO.BOARD)
+
+class myThread (threading.Thread):
+   def __init__(self, threadID, name, counter):
+      threading.Thread.__init__(self)
+      self.threadID = threadID
+      self.name = name
+      self.counter = counter
+   def run(self):
+      blink('yellow')
+
+def blink(color):
+    while True:
+        switch('yellow', 'on')
+        sleep(1)
+        switch('yellow', 'off')
+        sleep(1)
+        if stop_threads: 
+            break
 
 def switch(color, mode):
     if color == 'red':
@@ -26,11 +45,10 @@ def enableRed():
 def enableYellow():
     switch('green', 'off')
     switch('red', 'off')
-    while True:
-        switch('yellow', 'on')
-        sleep(1)
-        switch('yellow', 'off')
-        sleep(1)
+    thread1 = myThread(1, "Thread-1", 1)
+    thread1.start()
+    sleep(10)
+    stop_threads = True
 
 def enableGreen():
     switch('green', 'on')
